e9573c926fb65f04894dbbcab8c0f91ebcdd4f57
DESKTOP-VM38SOG\ismail <ciftcismail06@gmail.com>
2022-08-22 22:42:14 +0300

    +adjusment: spin settings

--- Assets/_WheelofFortune/Scripts/SpinWheel.cs
+++ Assets/_WheelofFortune/Scripts/SpinWheel.cs
@@ -3,67 +3,84 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-public class SpinWheel : MonoBehaviour
+
+namespace WheelofFortune.Wheel
 {
-    public List<AnimationCurve> animationCurves;
-
-    private bool spinning;
-    [SerializeField] private int _angle;
-    [SerializeField] private int _angleSpeed = 2;
-
-    void Start()
+    public class SpinWheel : MonoBehaviour
     {
-        spinning = false;
-        _angle = 45 * Random.Range(10, 50); 
-    }
+        [SerializeField] private SpinSettings _spinSettings;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && !spinning)
+        [SerializeField] private Button uiSpinButton;
+
+        private bool spinning;
+       
+
+        public List<string> Items;
+        public TextMeshProUGUI itemText;
+        public int initialIndex;
+
+        void Start()
         {
+            uiSpinButton.onClick.AddListener(() =>
+            {
+                if (!spinning)
+                {
+                    StartCoroutine((SpinTheWheel(_spinSettings.Angle, _spinSettings.AngleSpeed)));
+                }
 
-            StartCoroutine(SpinTheWheel( _angle, _angleSpeed));
-        }
-    }
+            });
 
-    IEnumerator SpinTheWheel(float endValue, float duration)
-    {
-        spinning = true;
-
-        float time = 0.0f;
-        float startValue = transform.eulerAngles.z;
-        
-
-        int animationCurveNumber = Random.Range(0, animationCurves.Count);
-        
-
-        while (time < duration)
-        {
-            float valueToChange = Mathf.Lerp(startValue, endValue, animationCurves[animationCurveNumber].Evaluate(time/duration));
-            transform.eulerAngles = new Vector3(0, 0, valueToChange);
-
-            time += Time.deltaTime;
-            yield return null;
-            //float angle = maxAngle * animationCurves[animationCurveNumber].Evaluate(timer / time);
-            //transform.eulerAngles = new Vector3(0.0f, 0.0f, angle + startAngle);
-            //timer += Time.deltaTime;
-            //yield return 0;
+            spinning = false;
+            _spinSettings.Angle = 45 * Random.Range(10, 50);
+            
         }
 
-        transform.eulerAngles = new Vector3(0.0f, 0.0f, endValue);
-        GiveItem();
-        _angle =  45 * Random.Range(10,50);
-        spinning = false; 
-    }
-    public List<string> Items;
-    public TextMeshProUGUI itemText;
-    public int initialIndex;
+        void Update()
+        {
+           
+            if (Input.GetKeyDown(KeyCode.Space) && !spinning)
+            {
 
-    void GiveItem()
-    {
-        int mainAngle = (int)(_angle) % 360;
-        int splitValue = (int)mainAngle / 45;
-        int currentIndex = ((initialIndex-splitValue)+8)% 8;
-        itemText.text = "" + Items[currentIndex];
+                StartCoroutine(SpinTheWheel(_spinSettings.Angle, _spinSettings.AngleSpeed));
+            }
+        }
+
+        IEnumerator SpinTheWheel(float endValue, float duration)
+        {
+            spinning = true;
+
+            float time = 0.0f;
+            float startValue = transform.eulerAngles.z;
+
+
+            int animationCurveNumber = Random.Range(0, _spinSettings.AnimationCurves.Count);
+
+
+            while (time < duration)
+            {
+                float valueToChange = Mathf.Lerp(startValue, endValue, _spinSettings.AnimationCurves[animationCurveNumber].Evaluate(time / duration));
+                transform.eulerAngles = new Vector3(0, 0, valueToChange);
+
+                time += Time.deltaTime;
+                yield return null;
+                //float angle = maxAngle * animationCurves[animationCurveNumber].Evaluate(timer / time);
+                //transform.eulerAngles = new Vector3(0.0f, 0.0f, angle + startAngle);
+                //timer += Time.deltaTime;
+                //yield return 0;
+            }
+
+            transform.eulerAngles = new Vector3(0.0f, 0.0f, endValue);
+            GiveItem();
+            _spinSettings.Angle = 45 * Random.Range(10, 50);
+            spinning = false;
+        }
+       
+        void GiveItem()
+        {
+            int mainAngle = (int)(_spinSettings.Angle) % 360;
+            int splitValue = (int)mainAngle / 45;
+            int currentIndex = ((initialIndex - splitValue) + 8) % 8;
+            itemText.text = "" + Items[currentIndex];
+        }
     }
 }
